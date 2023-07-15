c5a22195f98294cd903299aaaf60f3a840544430
Benito Serrano <benosg@gmail.com>
2023-07-15 16:59:46 -0400

    ejemplos basicos de python

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,11 @@
+import math
+print (round(1.3)) # redondeo
+print (abs(-55)) #valor absoluto
+
+print(math.ceil(1.1))
+print(math.floor(1.9999))
+print(math.isnan(23))
+print(math.pow(10,3))
+print(math.sqrt(9))
+
+#https://docs.python.org/3/library/math.html
---
+++ tipos/numeros.py
@@ -0,0 +1,12 @@
+numero = 2 #einteger
+decimal = 1.2 # float
+imaginario = 2+2j #2 + 2i
+
+numero += 2
+print ("numero",numero)
+print(1 + 3)
+print(1 - 3)
+print(1 / 3)
+print(1 // 3) # solo devuelve la parte entera de a division
+print(8 % 3) #mod
+print(2 ** 3) #potencia
